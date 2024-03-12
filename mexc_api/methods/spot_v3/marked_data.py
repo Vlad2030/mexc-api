f430b5ab9dbe029cdcdf5be1869bfc0e97c0f335
vladops <vlad123123v@gmail.com>
2024-03-12 16:50:59 +0300

    Update MarkedData

--- mexc_api/methods/spot_v3/marked_data.py
+++ mexc_api/methods/spot_v3/marked_data.py
@@ -21,6 +21,9 @@ class MarkedData:
     def __init__(self, client: MEXCClient) -> None:
         self.client = client
 
+    async def __aenter__(self) -> "MarkedData":
+        return self
+
     async def __aexit__(self) -> None:
         return await self.client.close_session()
 
@@ -30,6 +33,7 @@ class MarkedData:
             endpoint="/api/v3/ping",
             params={},
         )
+
         return request.response
 
     async def check_server_time(self) -> CheckServerTime:
@@ -38,6 +42,7 @@ class MarkedData:
             endpoint="/api/v3/time",
             params={},
         )
+
         return CheckServerTime(**to_snake_case(request.response))
 
     async def api_default_symbol(self, symbol: str = None) -> ApiDefaultSymbol:
@@ -46,11 +51,13 @@ class MarkedData:
             endpoint="/api/v3/defaultSymbols",
             params={"symbol": symbol,} if symbol is not None else None,
         )
+
         return ApiDefaultSymbol(**to_snake_case(request.response))
 
     async def exchange_information(self, symbols: str = None) -> ExchangeInformation:
         if symbols is not None:
             symbol = "symbols" if len(symbols.split(",")) > 1 else "symbol"
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/exchangeInfo",
@@ -58,10 +65,12 @@ class MarkedData:
                 symbol: symbols if symbol is not None else None,
             } if symbols is not None else {},
         )
+
         return ExchangeInformation(**to_snake_case(request.response))
 
     async def order_book(self, symbol: str, limit: int = 100) -> OrderBook:
         limit = 100 if limit > 5000 else limit
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/depth",
@@ -70,6 +79,7 @@ class MarkedData:
                 "limit": limit,
             },
         )
+
         return OrderBook(**to_snake_case(request.response))
 
     async def recent_trades_list(
@@ -79,6 +89,7 @@ class MarkedData:
     ) -> list[RecentTradesList]:
         list = []
         limit = 100 if limit > 1000 else limit
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/trades",
@@ -87,8 +98,10 @@ class MarkedData:
                 "limit": limit,
             },
         )
+
         for item in request.response:
             list.append(RecentTradesList(**to_snake_case(item)))
+
         return list
 
     async def compressed_aggregate_trades_list(
@@ -100,21 +113,27 @@ class MarkedData:
     ) -> list[CompressedAggregateTradesList]:
         list = []
         limit = 500 if limit > 1000 else limit
+
         params_dict = {
             "symbol": symbol,
             "limit": limit,
         }
+
         if start_time is not None:
             params_dict["startTime"] = start_time
+
         if end_time is not None:
             params_dict["endTime"] = end_time
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/aggTrades",
             params=params_dict,
         )
+
         for item in request.response:
             list.append(CompressedAggregateTradesList(**item))
+
         return list
 
     async def kline_candlestick_data(
@@ -126,20 +145,25 @@ class MarkedData:
             limit: int = 500,
     ) -> list[list]:
         limit = 500 if limit > 1000 else limit
+
         params_dict = {
             "symbol": symbol,
             "interval": interval,
             "limit": limit,
         }
+
         if start_time is not None:
             params_dict["startTime"] = start_time
+
         if end_time is not None:
             params_dict["endTime"] = end_time
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/klines",
             params=params_dict,
         )
+
         return request.response
 
     async def current_average_price(self, symbol: str) -> CurrentAveragePrice:
@@ -150,6 +174,7 @@ class MarkedData:
                 "symbol": symbol,
             },
         )
+
         return CurrentAveragePrice(**to_snake_case(request.response))
 
     async def day_ticker_price_change_statistics(
@@ -158,6 +183,7 @@ class MarkedData:
     ) -> DayTickerPriceChangeStatistics | list[DayTickerPriceChangeStatistics]:
         list = []
         symbol = symbol if symbol is not None else ""
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/ticker/24hr",
@@ -165,10 +191,13 @@ class MarkedData:
                 "symbol": symbol,
             },
         )
+
         if symbol == "":
             for item in request.response:
                 list.append(DayTickerPriceChangeStatistics(**to_snake_case(item)))
+
             return list
+
         return DayTickerPriceChangeStatistics(**to_snake_case(request.response))
 
     async def symbol_price_ticker(
@@ -177,6 +206,7 @@ class MarkedData:
     ) -> SymbolPriceTicker | list[SymbolPriceTicker]:
         list = []
         symbol = symbol if symbol is not None else ""
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/ticker/price",
@@ -184,10 +214,13 @@ class MarkedData:
                 "symbol": symbol,
             },
         )
+
         if symbol == "":
             for item in request.response:
                 list.append(SymbolPriceTicker(**to_snake_case(item)))
+
             return list
+
         return SymbolPriceTicker(**to_snake_case(request.response))
 
     async def symbol_order_book_ticker(
@@ -196,6 +229,7 @@ class MarkedData:
     ) -> SymbolOrderBookTicker | list[SymbolOrderBookTicker]:
         list = []
         symbol = symbol if symbol is not None else ""
+
         request = await self.client.request(
             method="GET",
             endpoint="/api/v3/ticker/bookTicker",
@@ -203,8 +237,11 @@ class MarkedData:
                 "symbol": symbol,
             },
         )
+
         if symbol == "":
             for item in request.response:
                 list.append(SymbolOrderBookTicker(**to_snake_case(item)))
+
             return list
+
         return SymbolOrderBookTicker(**to_snake_case(request.response))
