625fa4452750bc6f915cbd1dcd0332dc46cf478f
vladops <vlad123123v@gmail.com>
2024-03-16 22:09:42 +0300

    Add new schemas

--- mexc_api/types/spot_account_trade.py
+++ mexc_api/types/spot_account_trade.py
@@ -1,3 +1,4 @@
+from mexc_api.enums import OrderSide, OrderType
 from mexc_api.types.base_schema import BaseSchema
 
 class UserApiDefaultSymbol(BaseSchema):
@@ -15,3 +16,40 @@ class NewOrder(BaseSchema):
     type: str
     side: str
     transact_time: int
+
+
+class Order(BaseSchema):
+    symbol: str
+    side: OrderSide
+    type: OrderType
+    quantity: float | None = None
+    quote_order_qty: float | None = None
+    price: float | None = None
+    order_id: str = None
+    orig_client_order_id: str | None = None
+    new_client_order_id: str | None = None
+
+
+class BatchOrders(BaseSchema):
+    symbol: str | None
+    order_id: str | None
+    order_list_id: int | None
+    new_client_order_list_id: str | None
+    msg: str | None
+    code: int | None
+
+
+class CancelOrder(BaseSchema):
+    symbol: str
+    orig_client_order_id: str
+    order_id: int 
+    client_order_id: str
+    price: str
+    orig_qty: str
+    executed_qty: str
+    commulative_quote_qty: str
+    status: str
+    time_in_force: str
+    type: OrderType
+    side: OrderSide
+
